99d8a39451d031d70907fde18d5f99582e8538f7
Gwen Ewasko <gwenewasko@gmail.com>
2022-03-17 18:32:16 -0500

    finishing update function

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const db = mysql.createConnection(
   console.log(`Connected to the company_db database.`)
 );
 
-// Start by asking questions
-// View all employees
-// Inquirer prompt
 function init() {
   inquirer
     .prompt([
@@ -41,20 +38,44 @@ function init() {
             value: "ADD_EMPLOYEES",
           },
           {
+            name: "Add a department",
+            value: "ADD_DEPARTMENT",
+          },
+          {
+            name: "Add a role",
+            value: "ADD_ROLE",
+          },
+          {
+            name: "Update an employee role",
+            value: "UPDATE_ROLE",
+          },
+          {
             name: "Quit",
             value: "QUIT",
+            message: "Ok, you're all done!"
           },
         ],
         message: "What would you like to do?",
       },
     ])
     .then((answers) => {
+      // console.log(answers);
       if (answers.list === "VIEW_EMPLOYEES") {
         viewEmployees();
       } else if (answers.list === "VIEW_DEPARTMENTS") {
         viewDepartments();
+      } else if (answers.list === "VIEW_ROLES") {
+        viewRoles();
       } else if (answers.list === "ADD_EMPLOYEES") {
         createEmployee();
+      } else if (answers.list === "ADD_DEPARTMENT") {
+        createDepartment();
+      } else if (answers.list === "ADD_ROLE") {
+        createRole();
+      } else if (answers.list === "UPDATE_ROLE") {
+        updateRole();
+      } else if (answers.list === "QUIT") {
+        return "Ok, you're all done!";
       }
     });
 }
@@ -69,12 +90,14 @@ function viewEmployees() {
 function viewDepartments() {
   db.query("SELECT * FROM department", function (err, results) {
     console.table(results);
+    init();
   });
 }
 
 function viewRoles() {
   db.query("SELECT * FROM roles", function (err, results) {
     console.table(results);
+    init();
   });
 }
 
@@ -133,9 +156,139 @@ function createEmployee() {
       });
     });
 }
-// Inquirer prompt to ask for first, last name
-// .then query roles table
-// Inquirer prompt for role they want to choose
-// .then ask who the manager is
+
+function createDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "departmentName",
+        type: "input",
+        message: "What's the department you'd like to add?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        "INSERT INTO department(department_name) values(?)",
+        answers.departmentName
+      );
+
+      init();
+    });
+}
+
+function createRole() {
+  inquirer
+    .prompt([
+      {
+        name: "roleName",
+        type: "input",
+        message: "What's the role you'd like to add?",
+      },
+      {
+        name: "roleSalary",
+        type: "input",
+        message: "What's the salary for the role?",
+      },
+    ])
+    .then((answers) => {
+      db.query("SELECT * FROM department", function (err, results) {
+        const department = results.map(({ id, department_name }) => ({
+          name: department_name,
+          value: id,
+        }));
+        inquirer
+          .prompt({
+            type: "list",
+            name: "id",
+            message: "What department does this role belong in?",
+            choices: department,
+          })
+          .then((department) => {
+            db.query(
+              "INSERT INTO roles(title, salary, department_id) values(?,?,?)",
+              [answers.roleName, answers.roleSalary, department.id]
+            );
+            init();
+          });
+      });
+    });
+}
+
+function updateRole() {
+  db.query("SELECT * FROM employee", function (err, results) {
+    const employees = results.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id,
+    }));
+    db. query("SELECT * FROM roles", function (err, results) {
+      const roles = results.map(({id, title}) => ({
+        name: title,
+        value: id
+      }))
+      inquirer
+      .prompt([
+        {
+          name: "employeeId",
+          type: "list",
+          message: "Which employee would you like to update?",
+          choices: employees,
+        },
+        {
+          name: "newRole",
+          type: "list",
+          message: "What is the employee's new role?",
+          choices: roles,
+        },
+      ])
+      .then((answers) => {
+        db.query("UPDATE employee SET role_id = ? where id = ?", [answers.newRole, answers.employeeId], function (err, results) {
+        })
+        init();
+      });
+    });
+
+  });
+  // db.query("SELECT * FROM employee", function (err, results) {
+  //   const employee = results.map(
+  //     ({ id, first_name, last_name, role_id, manager_id }) => ({
+  //       name: title,
+  //       value: id,
+  //     })
+  //   );
+  //   inquirer
+  //     .prompt({
+  //       type: "list",
+  //       name: "id",
+  //       message: "What is the employee's role?",
+  //       choices: roles,
+  //     })
+  //     .then((role) => {
+  //       db.query(
+  //         "SELECT * FROM employee where manager_id is null",
+  //         function (err, results) {
+  //           const managers = results.map(({ id, last_name }) => ({
+  //             name: last_name,
+  //             value: id,
+  //           }));
+  //           inquirer
+  //             .prompt({
+  //               type: "list",
+  //               name: "id",
+  //               message: "What is the manager's name?",
+  //               choices: managers,
+  //             })
+  //             .then((manager) => {
+  //               db.query(
+  //                 "INSERT INTO employee(first_name, last_name, role_id, manager_id) values(?,?,?,?)",
+  //                 [answers.firstName, answers.lastName, role.id, manager.id]
+  //               );
+  //               init();
+  //             });
+  //         }
+  //       );
+  //     });
+  // });
+}
+
 
 init();
